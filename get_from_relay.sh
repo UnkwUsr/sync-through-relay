2647b91936ce8b884017685d167afcf8603a223d
UnkwUsr <ktoto2707043@gmail.com>
2023-11-01 20:12:00 +0300

    feat(get_from_relay): untar

--- get_from_relay.sh
+++ get_from_relay.sh
@@ -9,7 +9,11 @@ RELAY_SERVER="mys:~/termux-inbox"
 rsync --remove-source-files --ignore-missing-args \
     "$RELAY_SERVER/*" \
     "$LOCAL_DIR_SAVE/"
+# decrypt
 gpg -q --decrypt-files $LOCAL_DIR_SAVE/*.gpg
-rm $LOCAL_DIR_SAVE/*.gpg
+# untar, if have any
+tar xvf $LOCAL_DIR_SAVE/*.tar -C $LOCAL_DIR_SAVE --one-top-level
+
+rm $LOCAL_DIR_SAVE/*.gpg $LOCAL_DIR_SAVE/*.tar
 
 ls -1 $LOCAL_DIR_SAVE
