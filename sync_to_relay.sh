5dcaa601ce5124cb4aee42a9da04e9b1c2354013
UnkwUsr <ktoto2707043@gmail.com>
2023-11-01 16:07:48 +0300

    feat(sync_to_relay): send voices

--- sync_to_relay.sh
+++ sync_to_relay.sh
@@ -3,23 +3,34 @@ set -euo pipefail
 
 # TODO: absolute path
 PHONE_INBOX="$HOME/device/phone_inbox.md"
+PHONE_VOICES="$HOME/device/inbox_voices/"
 LOCAL_DIR_SENT="$HOME/device/sent"
 SYNC_TARGET="mys:~/termux-inbox"
 GPG_ID="for-termux-inbox"
 
 newfilename="inbox_$EPOCHSECONDS.md"
+newfilename_voices="voices_$EPOCHSECONDS"
 cryptedfile=$(mktemp -u)
+crypted_voices=$(mktemp -u)
 # crypt
 gpg --encrypt -o "$cryptedfile" --recipient $GPG_ID "$PHONE_INBOX"
+# crypt voices
+gpgtar --encrypt -o "$crypted_voices" --recipient $GPG_ID \
+    -C "$(dirname "$PHONE_VOICES")" "$(basename "$PHONE_VOICES")"
 # send
 scp "$cryptedfile" "$SYNC_TARGET/$newfilename.gpg" \
     || (echo >> "$PHONE_INBOX" && exit 1)
-# remove crypted temp file
-rm "$cryptedfile"
+# send voices
+scp "$crypted_voices" "$SYNC_TARGET/$newfilename_voices.tar.gpg" \
+    || (echo -e "\ncant send voices\n" >> "$PHONE_INBOX" && exit 1)
+# remove crypted temp files
+rm "$cryptedfile" "$crypted_voices"
 
-sentfile="$LOCAL_DIR_SENT/$newfilename"
 mkdir -p "$LOCAL_DIR_SENT"
-mv -n "$PHONE_INBOX" "$sentfile"
+# move inbox to sent
+mv -n "$PHONE_INBOX" "$LOCAL_DIR_SENT/$newfilename"
+# move voices to sent
+mv "$PHONE_VOICES" "$LOCAL_DIR_SENT/$newfilename_voices"
 
 # TODO: god, should gurantee that sync did well
 # UPD: would like to live some time with it, see how it goes, later can add. Ok
