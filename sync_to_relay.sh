9c4a7450d484130746062554f6e1063f7f12fed2
UnkwUsr <ktoto2707043@gmail.com>
2023-11-06 01:06:30 +0300

    feat(sync_to_relay): make one archive/send per sync
    refactor, breaking change

--- sync_to_relay.sh
+++ sync_to_relay.sh
@@ -2,37 +2,13 @@
 set -euo pipefail
 
 INBOX_FOLDER="$HOME/device/txts/phone_inbox"
-
-INBOX_MD="$INBOX_FOLDER/inbox.md"
-INBOX_VOICES="$INBOX_FOLDER/voices/"
-LOCAL_DIR_SENT="$INBOX_FOLDER/sent"
+LOCAL_DIR_SENT="$HOME/device/txts/phone_inbox_sent"
 
 RELAY_SERVER="mys:~/termux-inbox"
 GPG_ID="for-termux-inbox"
 
-append_time() {
-    if [ -d "$1" ]; then
-        echo "${1}_$EPOCHSECONDS"
-    else
-        echo "${EPOCHSECONDS}_$1"
-    fi
-}
-
-crypt_into() {
-    src="$1"
-    new="$2"
-    if [ -d "$src" ]; then
-        crypted="$TMPDIR/$new.tar.gpg"
-        (cd "$src" && \
-            gpgtar --encrypt -o "$crypted" --recipient $GPG_ID ./*)
-    else
-        crypted="$TMPDIR/$new.gpg"
-        gpg --encrypt -o "$crypted" --recipient $GPG_ID "$src"
-    fi
-    echo "$crypted"
-}
-
-# crypt, send and move to folder "sent" (just in case)
+# crypt content of folder and send. If ok, then sent content moved to "sent"
+# folder (just in case, instead of deleting)
 crypt_and_send() {
     src="$1"
     if [ ! -e "$src" ]; then
@@ -40,20 +16,19 @@ crypt_and_send() {
         return
     fi
 
-    timed_name="$(append_time "$(basename "$src")")"
-
-    crypted=$(crypt_into "$src" "$timed_name")
+    time="$EPOCHSECONDS"
+    crypted="$TMPDIR/${time}.tar.gpg"
+    tar cv -C "$src" . | gpg --encrypt --recipient "$GPG_ID" -o "$crypted"
 
     scp "$crypted" "$RELAY_SERVER/" \
-        || (echo >> "$INBOX_MD" && echo "can't send" && exit 1)
+        || (echo "can't send: $time" >> "$src/errors" && exit 1)
     rm "$crypted"
 
     mkdir -p "$LOCAL_DIR_SENT"
-    mv -n "$src" "$LOCAL_DIR_SENT/$timed_name"
+    mv -n "$src" "$LOCAL_DIR_SENT/$time"
 }
 
-crypt_and_send "$INBOX_MD"
-crypt_and_send "$INBOX_VOICES"
+crypt_and_send "$INBOX_FOLDER"
 
 echo
 echo "all done. Press enter"
